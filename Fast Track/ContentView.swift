9ac294ebeb55c9d5040a780ad34abba134d1d54b
Dominik Moreira <dominik.privatacc@gmail.com>
2026-06-21 19:26:17 +0200

    First layout

--- Fast Track/ContentView.swift
+++ Fast Track/ContentView.swift
@@ -11,12 +11,29 @@ import Playgrounds
 
 struct ContentView: View{
     let gridItems: [GridItem] = [
-        GridItem(.fixed(100)),
-        GridItem(.fixed(100)),
-        GridItem(.fixed(100))
+        GridItem(.adaptive(minimum: 150, maximum: 200)),
     ]
     
+    @AppStorage("searchText") var searchText = ""
+    
     var body: some View {
+        VStack {
+            HStack {
+                TextField("Search for a Song", text: $searchText)
+                Button("Search") {
+                    // action code
+                }
+            }
+        }
+        
+        ScrollView {
+            LazyVGrid(columns: gridItems) {
+                ForEach(1..<100) { i in
+                    Color.red
+                        .frame(width: 150, height: 150)
+                }
+            }
+        }
     }
 }
 
